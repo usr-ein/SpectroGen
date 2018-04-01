56f2cf127b652eb0753e80239756019b65644931
sam1902 <sam1902@me.com>
2018-04-01 20:07:33 +0200

    Rename spectroGenV3.py to spectroGen.py

--- spectroGen.py
+++ spectroGen.py
@@ -1,51 +1,153 @@
-import wave, struct, math
-from random import randint
+import wave, struct, math # To calculate the WAV file content
+import numpy as np # To handle matrices
+from PIL import Image # To open the input image and convert it to grayscale
 
-sampleRate = 44100.0 # hertz
-duration = 1.0       # seconds
-frequency = 500.0    # hertz
+import scipy.ndimage # To resample using nearest neighbour
 
-wavef = wave.open('sound.wav','w')
-wavef.setnchannels(1) # mono
-wavef.setsampwidth(2) 
-wavef.setframerate(sampleRate)
-
-max_i = int(duration * sampleRate)
-swapRect = 1
-for i in range(max_i):
-    #value = int(32767.0*math.cos(frequency*math.pi*float(i)/float(sampleRate)))
-    #value = 32767
-    #if int(duration * sampleRate) % 2 == 0:
-    #    value *= -1
-    #value = int(i*2*32767/int(duration*sampleRate)/2)
-    #value = int(int(duration * sampleRate) % (i/int(duration * sampleRate)*9000 + 1) * 3.2767 )
-
-    # Sawtooth
-    #value = int(i % (max_i/100) / (max_i/100) * 32767)
-
-    # Square signal
-    '''
-    if math.floor(i/max_i*10) % 2 == 0:
-        swapRect = -1
+'''
+    Loads a picture, converts it to greyscale, then to numpy array, normalise it so that the max value is 1 
+    the min is 0, increase the contrast a bit, remove every pixel which intensity is lower that 0.5, 
+    then resize the picture using nearest neighbour resampling and outputs the numpy matrix.
+    
+    FYI: imgArr[0,0] is the top left corner of the image, cheers matrix indexing
+    
+    Returns: the resized image as a high contrast, normalised between 0 and 1, numpy matrix
+'''
+def loadPicture(size, file, contrast=True, highpass=False, verbose=1):
+    img = Image.open(file)
+    img = img.convert("L")
+    #img = img.resize(size) # DO NOT DO THAT OR THE PC WILL CRASH
+    
+    imgArr = np.array(img)
+    imgArr = np.flip(imgArr, axis=0)
+    if verbose:
+        print("Image original size: ", imgArr.shape)
+        
+    # Increase the contrast of the image
+    if contrast:
+        imgArr = 1/(imgArr+10**15.2) # Now only god knows how this works but it does
     else:
-        swapRect = 1
+        imgArr = 1 - imgArr
+    # Scale between 0 and 1
+    imgArr -= np.min(imgArr)
+    imgArr = imgArr/np.max(imgArr)
+    # Remove low pixel values (highpass filter)
+    if highpass:
+        removeLowValues = np.vectorize(lambda x: x if x > 0.5 else 0, otypes=[np.float])
+        imgArr = removeLowValues(imgArr)
 
-    value = 0 if swapRect < 0 else 32767
-    '''
+    if size[0] == 0:
+        size = imgArr.shape[0], size[1]
+    if size[1] == 0:
+        size = size[0], imgArr.shape[1]
+    resamplingFactor = size[0]/imgArr.shape[0], size[1]/imgArr.shape[1]
+    if resamplingFactor[0] == 0:
+        resamplingFactor = 1, resamplingFactor[1]
+    if resamplingFactor[1] == 0:
+        resamplingFactor = resamplingFactor[0], 1
     
-    # White noise
-    #value = randint(-32767-1, 32767)
+    # Order : 0=nearestNeighbour, 1:bilinear, 2:cubic etc...
+    imgArr = scipy.ndimage.zoom(imgArr, resamplingFactor, order=0)
     
-    # Bi-band noise
-    f = 900 if i % 2 == 0 else 4000
-    value = 32767.0*math.cos(f * math.pi * float(i) / float(sampleRate))
-    value = int(value)
-    print(value, end=" - ")
-    # Short int, little endian
-    data = struct.pack('<h', value)
-    wavef.writeframesraw( data )
+    if verbose:
+        print("Resampling factor", resamplingFactor)
+        print("Image resized :", imgArr.shape)
+        print("Max intensity: ", np.max(imgArr))
+        print("Min intensity: ", np.min(imgArr))
+    return imgArr
 
-wavef.writeframes(''.encode())
-wavef.close()
+def genSoundFromImage(file, output="sound.wav", duration=5.0, sampleRate=44100.0, intensityFactor=1, min_freq=0, max_freq=22000, invert=False, contrast=True, highpass=True, verbose=False):
+    wavef = wave.open(output,'w')
+    wavef.setnchannels(1) # mono
+    wavef.setsampwidth(2) 
+    wavef.setframerate(sampleRate)
+    
+    max_frame = int(duration * sampleRate)
+    max_intensity = 32767 # Defined by WAV
+    
+    stepSize = 400 # Hz, each pixel's portion of the spectrum
+    steppingSpectrum = int((max_freq-min_freq)/stepSize)
+    
+    imgMat = loadPicture(size=(steppingSpectrum, max_frame), file=file, contrast=contrast, highpass=highpass, verbose=verbose)
+    if invert:
+        imgMat = 1 - imgMat
+    imgMat *= intensityFactor # To lower/increase the image overall intensity
+    imgMat *= max_intensity # To scale it to max WAV audio intensity
+    if verbose:
+        print("Input: ", file)
+        print("Duration (in seconds): ", duration)
+        print("Sample rate: ", sampleRate)
+        print("Computing each soundframe sum value..")
+    for frame in range(max_frame):
+        if frame % 60 == 0: # Only print once in a while
+            print("Progress: ==> {:.2%}".format(frame/max_frame), end="\r")
+        signalValue, count = 0, 0
+        for step in range(steppingSpectrum):
+            intensity = imgMat[step, frame]
+            if intensity < 0.1*intensityFactor:
+                continue
+            # nextFreq is less than currentFreq
+            currentFreq = (step * stepSize) + min_freq
+            nextFreq = ((step+1) * stepSize) + min_freq
+            if nextFreq - min_freq > max_freq: # If we're at the end of the spectrum
+                nextFreq = max_freq
+            for freq in range(currentFreq, nextFreq, 1000): # substep of 1000 Hz is good
+                signalValue += intensity*math.cos(freq * 2 * math.pi * float(frame) / float(sampleRate))
+                count += 1
+        if count == 0: count = 1
+        signalValue /= count
+        
+        data = struct.pack('<h', int(signalValue))
+        wavef.writeframesraw( data )
+        
+    wavef.writeframes(''.encode())
+    wavef.close()
+    print("\nProgress: ==> 100%")
+    if verbose:
+        print("Output: ", output)
 
+import sys
+import argparse
 
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputImage", help="Input image in any PIL supported format (JPG, PNG (with and without alpha), BMP etc...)")
+    parser.add_argument("outputFile", help="path where to output the soundfile in WAV format")
+    parser.add_argument("-d", "--duration", help="Duration of the sound to output, in whole seconds, default: 5", type=int)
+    parser.add_argument("-n", "--minFreq", help="Minimum frequency to use, in Hz, default: 0", type=int)
+    parser.add_argument("-x", "--maxFreq", help="Maximum frequency to use, in Hz, default: 22000", type=int)
+    parser.add_argument("-s", "--samplerate", help="Sample rate of the sound to output, in Hertz, default: 44100", type=int)
+    parser.add_argument("-if", "--intensityFactor", help="Factory by which multiply the image intensity, in decimal, default: 1.0", type=float)
+    parser.add_argument("-i", "--invert", help="Invert the image intensity, resulting in an inverted spectrum", action="store_true")
+    parser.add_argument("-c", "--contrast", help="Increases image's contrast before converting it, can enhance the resulting spectrum", action="store_true")
+    parser.add_argument("-hi", "--highintensity", help="Cut low intensity pixels, can enhance result", action="store_true")
+    parser.add_argument("-v", "--verbose", help="Display verbose", action="store_true")
+    args = parser.parse_args()
+    
+    img = args.inputImage
+    output = args.outputFile
+    duration = 5 if not args.duration else args.duration
+    min_freq = 0 if not args.minFreq else args.minFreq
+    max_freq = 22000 if not args.maxFreq else args.maxFreq
+    sampleRate = 44100 if not args.samplerate else args.samplerate
+    intensityFactor = 1 if not args.intensityFactor else args.intensityFactor
+    invert = args.invert
+    contrast = args.contrast
+    highpass = args.highintensity # Not a real highpass, but it cuts low intensities...
+    verbose = args.verbose
+
+    genSoundFromImage(
+            file=img, 
+            output=output, 
+            duration=duration, 
+            sampleRate=sampleRate,
+            min_freq=min_freq,
+            max_freq=max_freq,
+            contrast=contrast, 
+            invert=invert, 
+            intensityFactor=intensityFactor,
+            highpass=highpass, 
+            verbose=verbose)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
