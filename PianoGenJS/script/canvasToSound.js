9ae51ff5c528f76c70a312f1be9194bfe8a4dfc8
sam1902 <sam1902@me.com>
2018-04-06 20:27:13 +0200

    Updated canvasToSound.js to use Web Audio API to generate notes from list of drawn points

--- PianoGenJS/script/canvasToSound.js
+++ PianoGenJS/script/canvasToSound.js
@@ -1,68 +1,127 @@
+var context;
+var bufferLoader;
 
-function getAlphaAtPos(x,y,canvasData,canvasWidth){
-	let index = (y * canvasWidth + x) * 4;
-	// Alpha (red = +0, green = +1, blue = +2, alpha = +3)
-	return canvasData.data[index + 3];
+let twelfthSqrt2 = 1.05946; // To compute freqs
+
+var noteFreq = {
+	ut : 261.6, // = Do
+	re : 293.7,	
+	mi : 329.6,	
+	fa : 349.2,	
+	sol : 392,	
+	la : 440,	
+	si : 493.9 
 }
 
-function soundCanvas(canvas, duration, max_freq) {
-	let ctx = canvas.getContext('2d');
-	// Very costly in ressources, do that just once
-	let canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+var noteColour = {
+	ut : hexToRgb("#00FF00"), // = Do
+	re : hexToRgb("#FFFF00"),	
+	mi : hexToRgb("#FF1500"),	
+	fa : hexToRgb("#FF0000"),	
+	sol : hexToRgb("#FFC0CB"),	
+	la : hexToRgb("#800080"),	
+	si : hexToRgb("#0000FF") 
+}
 
-	var wave = new RIFFWAVE();
-	var data = [];
-	wave.header.sampleRate = 44100;
-	let max_frame = wave.header.sampleRate * duration;
-	let width = canvas.width;
-	let height = canvas.height;
+// keyNumber: from 1 to 88
+function getFreq(keyNumber){
+	return Math.pow(twelfthSqrt2, keyNumber-49) * 440;
+}
 
-	for (var frame = 0; frame < max_frame; frame++) {
-		var sumVal = 0;
-		var count = 0;
-		for (var y = 0; y < height; y += 1){
-			let intensity = getAlphaAtPos(Math.ceil(frame*width/max_frame), y, canvasData, width)/255;
-			if (intensity <= 0.0001)
-				continue;
-			// Takes the opposite of the freq you would expect because the image is upside down
-			//let freq = max_freq - y*max_freq/height;
-			// Same as above, but I wanted to experiment
-			let freq = (height-y)*max_freq/height;
-
-			sumVal += 128 + 127 * intensity * Math.cos(2 * frame * Math.PI * freq / wave.header.sampleRate);
-			count += 1;
+function freqFromColour(r, g, b){
+	var distances = [];
+	for (var note in noteColour){
+		colour = noteColour[note];
+		dist = Math.sqrt(Math.pow(colour.r - r, 2) + Math.pow(colour.g - g, 2) + Math.pow(colour.b - b, 2));
+		distances[note] = dist;
+	}
+	var closestNote;
+	var closestDist = 1500; // Large distance
+	for (var note in distances){
+		if (distances[note] < closestDist){
+			closestDist = distances[note];
+			closestNote = note;
 		}
-		if (count == 0){
-			count = 1;
-		}
-		data[frame] = Math.round(sumVal/count);
+	}
+	return noteFreq[closestNote];
+}
+
+function playSoundBuffer(buffer, time) {
+	var source = context.createBufferSource();
+	source.buffer = buffer;
+	source.connect(context.destination);
+	source.start(time);
+}
+
+function playSoundOscillator(freq, time) {
+	var osc = context.createOscillator();
+	osc.frequency.value = freq;
+	osc.type = "triangle";
+	osc.start(time);
+	osc.stop(time + 0.30);
+	
+	var gainNode = context.createGain();
+	gainNode.gain.setValueAtTime(1.0, time);
+	gainNode.gain.exponentialRampToValueAtTime(0.01, time + 0.30);
+
+	osc.connect(gainNode);
+	gainNode.connect(context.destination);
+}
+
+function playCanvas(bufferList){
+	// canvasPointList is declared in sketchpad.js in global scope
+	// it contains a list of each notes position with their colour
+	var do_piano = bufferList[0];
+
+	var startTime = context.currentTime + 0.100;
+
+	var tempo = 120; // BPM
+	var quarterNoteTime = 60/tempo;
+
+	for (var i = 0; i < canvasPointList.length; i++){
+		var dot = canvasPointList[i];
+		let noteTime = startTime + dot.x/300 * quarterNoteTime;
+		//playSoundBuffer(do_piano, noteTime);
+
+		//playSoundOscillator(noteFreq.la, noteTime);
+		//playSoundOscillator((1-dot.y/canvas.height)*2000, noteTime);
+		//playSoundOscillator(freqFromColour(dot.r, dot.g, dot.b), noteTime);
+		//var freq = freqFromColour(dot.r, dot.g, dot.b);
+		//var octave = Math.ceil((canvas.height-dot.y)/(canvas.height/5));
+		//playSoundOscillator(freq*octave, noteTime);
+		playSoundOscillator(getFreq(Math.round((1-dot.y/canvas.height)*88)), noteTime);
+	}
+}
+
+function play() {
+	let duration = document.getElementById('soundDuration').value;
+	playCanvas(bufferLoader.bufferList);
+}
+
+function save(filename) {
+	let canvas = document.getElementById('sketchpad');
+	let duration = document.getElementById('soundDuration').value;
+}
+
+function initSound(){
+	try {
+		context = new AudioContext();
+	}
+	catch(e) {
+		alert("Web Audio API is not supported in this browser");
 	}
 
-	wave.Make(data);
-	return wave;
-}
-function playSound() {
-	let canvas = document.getElementById('sketchpad');
-	let duration = document.getElementById('soundDuration').value;
-	let maxFreq = document.getElementById('maxFreq').value;
-	var wave = soundCanvas(canvas, duration, maxFreq);
-	var audio = new Audio();
-	audio.src = wave.dataURI;
-	audio.play();
+	// Start loading the piano sound.
+	bufferLoader = new BufferLoader(
+			context,
+			[
+			"sound/do_piano.wav"
+			],
+			bufferLoadCompleted  
+			);
+
+	bufferLoader.load();
 }
 
-function saveSound(filename) {
-	let canvas = document.getElementById('sketchpad');
-	let duration = document.getElementById('soundDuration').value;
-	let maxFreq = document.getElementById('maxFreq').value;
-	let uri = soundCanvas(canvas, duration, maxFreq).dataURI;
-	// Construct the <a> element
-	var link = document.createElement("a");
-	link.download = filename;
-	// Construct the uri
-	link.href = uri;
-	document.body.appendChild(link);
-	link.click();
-	// Cleanup the DOM
-	document.body.removeChild(link);
+function bufferLoadCompleted() {		
 }
